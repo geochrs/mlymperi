a9da5a28eacadcfc8bfc5d6c09d063b07e6cbd7b
geochrs <geo.christopoulos@hotmail.com>
2024-12-12 00:21:48 +0200

    added basename

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Root from './pages/rOOT.JSX';
 import HomePage from './pages/HomePage';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Root />,
-      children: [{ index: true, element: <HomePage /> }],
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <Root />,
+        children: [{ index: true, element: <HomePage /> }],
+      },
+    ],
+    { basename: '/mlymperi' } // Add basename here
+  );
 
   return <RouterProvider router={router} />;
 }
