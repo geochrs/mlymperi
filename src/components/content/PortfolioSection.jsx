1d5bdd5443045d2ffebaeb8da2b7b9964330cfb4
geochrs <geo.christopoulos@hotmail.com>
2024-12-13 00:48:15 +0200

    added first cards

--- src/components/content/PortfolioSection.jsx
+++ src/components/content/PortfolioSection.jsx
@@ -6,7 +6,64 @@ export default function PortfolioSection() {
       <div className={classes.container}>
         <h2 className={classes.h2}>Portfolio</h2>
         <p>A small sample of the logos I have designed.</p>
-        <div className={classes.cards}></div>
+        <div className={classes.cardsContainer}>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 1"
+              className={classes.image}
+            />
+          </div>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 2"
+              className={classes.image}
+            />
+          </div>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 3"
+              className={classes.image}
+            />
+          </div>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 3"
+              className={classes.image}
+            />
+          </div>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 3"
+              className={classes.image}
+            />
+          </div>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 3"
+              className={classes.image}
+            />
+          </div>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 3"
+              className={classes.image}
+            />
+          </div>
+          <div className={classes.card}>
+            <img
+              src="https://via.placeholder.com/300"
+              alt="Card 3"
+              className={classes.image}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
