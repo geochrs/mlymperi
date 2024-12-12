ed2644c559251ef3af463161bec6bacbc03ab45b
geochrs <geo.christopoulos@hotmail.com>
2024-12-12 23:30:36 +0200

    portfolio title and css

--- src/components/content/PortfolioSection.jsx
+++ src/components/content/PortfolioSection.jsx
@@ -5,6 +5,7 @@ export default function PortfolioSection() {
     <section className={classes.section}>
       <div className={classes.container}>
         <h2 className={classes.h2}>Portfolio</h2>
+        <p>A small sample of the logos I have designed.</p>
         <div className={classes.cards}></div>
       </div>
     </section>
