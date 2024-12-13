a10a56cab7433a801ee4fe02f00013e768039156
geochrs <geo.christopoulos@hotmail.com>
2024-12-13 10:40:14 +0200

    added scroll functionality for the portfolio and navigate from other link back to portfolio

--- src/components/content/PortfolioSection.jsx
+++ src/components/content/PortfolioSection.jsx
@@ -2,7 +2,7 @@ import classes from './PortfolioSection.module.css';
 
 export default function PortfolioSection() {
   return (
-    <section className={classes.section}>
+    <section id='portfolio' className={classes.section}>
       <div className={classes.container}>
         <h2 className={classes.h2}>Portfolio</h2>
         <p>A small sample of the logos I have designed.</p>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,9 +2,51 @@ import { NavLink, Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import classes from './Navbar.module.css';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [activeSection, setActiveSection] = useState('');
   const [isSticky, setIsSticky] = useState(false);
+
+  const handlePortfolioClick = () => {
+    navigate('/');
+
+    setTimeout(() => {
+      const portfolioSection = document.getElementById('portfolio');
+      if (portfolioSection) {
+        portfolioSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 500);
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.target.id === 'portfolio') {
+            if (entry.isIntersecting) {
+              setActiveSection('portfolio');
+            } else {
+              setActiveSection('');
+            }
+          }
+        });
+      },
+      { threshold: 0.4 }
+    );
+
+    if (location.pathname === '/') {
+      const portfolioSection = document.querySelector('#portfolio');
+      if (portfolioSection) {
+        observer.observe(portfolioSection);
+      }
+    }
+
+    return () => observer.disconnect();
+  }, [location.pathname]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,7 +71,15 @@ export default function Navbar() {
         </Link>
         <nav className={classes.navBar}>
           <ul className={classes.navList}>
-            <li>Portfolio</li>
+            <li>
+              <a
+                href="#"
+                onClick={handlePortfolioClick}
+                className={activeSection === 'portfolio' ? classes.active : ''}
+              >
+                Portfolio
+              </a>
+            </li>
             <li>
               <NavLink
                 to="/about"
