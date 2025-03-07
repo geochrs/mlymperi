8f0254efc6f8d4e7fb83bf4a6b4d6ce54189391c
geochrs <geo.christopoulos@hotmail.com>
2025-03-07 13:06:55 +0200

    changed positions at sections to add an effect

--- src/components/content/HeroSection.jsx
+++ src/components/content/HeroSection.jsx
@@ -1,7 +1,25 @@
 import classes from './HeroSection.module.css';
 import video from '../../assets/background.mp4';
+import { useState, useEffect } from 'react';
 
 export default function HeroSection() {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const fadeStart = 0;
+      const fadeEnd = 300;
+      let newOpacity = 1 - (scrollY - fadeStart) / (fadeEnd - fadeStart);
+      newOpacity = Math.max(newOpacity, 0);
+      setOpacity(newOpacity);
+      document.documentElement.style.setProperty("--opacity-value", newOpacity);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  
   return (
     <section className={classes.section}>
       <div className={classes.container}>
@@ -16,8 +34,12 @@ export default function HeroSection() {
             <source src={video} type="video/mp4" />
           </video>
           <h1 className={classes.h1}>
-            <span className={`${classes.color} ${classes.quotes}`}>&ldquo;</span>M<span className={classes.differentFont}>a</span>ke it simple,
-            <br />But unique<span className={classes.color}>.</span>
+            <span className={`${classes.color} ${classes.quotes}`}>
+              &ldquo;
+            </span>
+            M<span className={classes.differentFont}>a</span>ke it simple,
+            <br />
+            But unique<span className={classes.color}>.</span>
           </h1>
           <div className={classes.content}>
             <p>WELCOME</p>
