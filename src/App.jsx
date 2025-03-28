6a642e12308817df2d9b2af163c1d74f7685963d
geochrs <geo.christopoulos@hotmail.com>
2025-03-28 23:37:44 +0200

    typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,17 @@ import AboutPage from './pages/AboutPage';
 import DetailPage from './pages/DetailPage';
 
 function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/',
-        element: <Root />,
-        children: [
-          { index: true, element: <HomePage /> },
-          { path: '/about', element: <AboutPage /> },
-          { path: '/details/:id', element: <DetailPage /> },
-        ],
-      },
-    ],
-    { basename: '/mlymperi/' }
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: '/about', element: <AboutPage /> },
+        { path: '/details/:id', element: <DetailPage /> },
+      ],
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 }
